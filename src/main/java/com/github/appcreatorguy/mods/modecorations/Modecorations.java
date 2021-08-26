af3fd72098ec9097bc80599a2aecd52a267c961d
appcreatorguy <manmenonsense@gmail.com>
2021-08-26 12:36:13 +0100

    Initial commit

---
+++ src/main/java/com/github/appcreatorguy/mods/modecorations/Modecorations.java
@@ -0,0 +1,10 @@
+package com.github.appcreatorguy.mods.modecorations;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Modecorations implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/github/appcreatorguy/mods/modecorations/client/ModecorationsClient.java
@@ -0,0 +1,13 @@
+package com.github.appcreatorguy.mods.modecorations.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ModecorationsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
